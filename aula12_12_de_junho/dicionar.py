13111253965cb06ec14b39eac34dd4a7fe41680b
thomasandyrsson <130843266+thomasandyrsson@users.noreply.github.com>
2024-06-12 11:03:17 -0300

    Add files via upload

---
+++ aula12_12_de_junho/dicionar.py
@@ -0,0 +1,8 @@
+produto = {
+    "Nome": "Teclado",
+    "Descrição": "Teclado Multi ER8569 com teclas direcionais e numéricas",
+    "Preço": 89.90,
+    "Quantidade": 28,
+}
+
+print(f"\nESTE PRODUTO CUSTA R$ {produto['Preço']} E HÁ {produto['Quantidade']} UNIDADES\n")
