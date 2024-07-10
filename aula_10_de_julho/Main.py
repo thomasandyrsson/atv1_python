b775b1fc77b0c8be1d5b215509e2829db4916446
thomasandyrsson <130843266+thomasandyrsson@users.noreply.github.com>
2024-07-10 19:03:35 +0000

    class Pokemon

--- aula_10_de_julho/Main.py
+++ aula_10_de_julho/Main.py
@@ -1,25 +1,31 @@
 from Conta import Conta
 
+#Instanciar novos objetos
 c1 = Conta("Thomas Andersson", 2842.99)
 c2 = Conta("Andersson Araujo", 1597.25)
 c3 = Conta("João Alfredo", 0)
 
+#novos depositos
 c1.depositar(500)
 c2.depositar(600)
 c3.depositar(3500)
 
+#novo saques
 c1.sacar()
 c2.sacar()
 c3.sacar()
 
+#informações
 c1.imprimeDados()
 c2.imprimeDados()
 c3.imprimeDados()
 
+#novos titulares
 c1.setTitular("Robert De Niro")
 c2.setTitular("Jennifer Anniston")
 c3.setTitular("Adriane Matias")
 
+#informações
 c1.imprimeDados()
 c2.imprimeDados()
 c3.imprimeDados()
